301849d0ad8ac67817542948e36408f590e41541
harrad <mattharrad@gmail.com>
2020-12-09 19:38:59 +0000

    Update 01.cs

--- 2020/01.cs
+++ 2020/01.cs
@@ -3,16 +3,23 @@ void Main()
 	var text = File.ReadAllLines(@"2020_12_01.txt");
 	var data = text.Select(x => int.Parse(x)).OrderBy(x=> x);	
 	
-	//QUESTION 1
-	var q1 = data.First(x => data.Contains(2020 - x));
-	($"Q1 : {q1 * (2020 - q1)}").Dump();
+	Question1(data).Dump();
+	Question2(data).Dump();
+}
 
-	//QUESTION 2
+long Question1(IEnumerable<int> data)
+{
+	var q1 = data.First(x => data.Contains(2020 - x));
+	return q1 * (2020 - q1);
+}
+
+long Question2(IEnumerable<int> data)
+{
 	foreach (var d in data)
 	{
 		var q2 = data.Where(x => x != d).FirstOrDefault(x => data.Contains(2020 - x - d));
-		if (q2 == 0) continue;		
-		($"Q2 : {d * q2 * (2020 - q2 - d)}").Dump();
-		break;		
+		if (q2 == 0) continue;
+		return d * q2 * (2020 - q2 - d);
 	}
+	return -1;
 }
